58f549cbc39a777d7d33d99fad7969af68da5d41
RahulChoudhary002 <Rahulchoudhary002@hotmail.com>
2025-04-05 16:06:33 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,11 @@
+def convert_tuple_to_list(input_tuple):
+    result_list = list(input_tuple)
+    return result_list
+
+# Example tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Convert and print
+my_list = convert_tuple_to_list(my_tuple)
+print("Original Tuple:", my_tuple)
+print("Converted List:", my_list)
